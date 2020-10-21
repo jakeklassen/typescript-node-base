cdd3aa6f2c4a601b645af7eccd40908463fb1bf4
Jake Klassen <jklassendev@gmail.com>
2020-10-21 08:39:18 -0400

    saving work

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,13 @@ import express from 'express';
 
 const app = express();
 
-console.log('🚀');
-
 console.log(`add(1, 2, 3, 4) = ${add(1, 2, 3, 4)}`);
 
 console.log(`clamp(12, 1, 100) = ${clamp(12, 1, 100)}`);
 
 app.get('/', (req, res) => res.json({ message: 'o hai!' }));
 
-app.listen(process.env.PORT || 3000);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is 🚀 on port ${port}`);
+});
