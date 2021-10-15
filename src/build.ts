8b8b18e0f2a18b3332d488b4b4d07d07b371898b
Jake Klassen <jklassendev@gmail.com>
2021-10-15 15:45:51 -0400

    messing around

--- src/build.ts
+++ src/build.ts
@@ -1,18 +1,24 @@
 import esbuild from 'esbuild';
+import path from 'node:path';
+import glob from 'tiny-glob';
 
-esbuild
-  .build({
-    target: 'node16',
-    platform: 'neutral',
-    format: 'esm',
-    write: true,
-    bundle: false,
-    outdir: 'dist',
-    sourcemap: 'external',
-    loader: {
-      '.ts': 'ts',
-    },
-    entryPoints: ['./src/index.ts'],
-    tsconfig: './tsconfig.json',
-  })
-  .catch(console.error);
+console.log(path.resolve(import.meta.url, '..'));
+
+(async () => {
+  esbuild
+    .build({
+      target: 'node16',
+      platform: 'node',
+      format: 'esm',
+      write: true,
+      bundle: false,
+      outdir: 'dist',
+      sourcemap: 'external',
+      loader: {
+        '.ts': 'ts',
+      },
+      entryPoints: await glob('./src/**/*.ts'),
+      tsconfig: './tsconfig.json',
+    })
+    .catch(console.error);
+})();
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,36 @@
 import { add, clamp } from '@app/lib/math';
-import express from 'express';
+import express, { ErrorRequestHandler } from 'express';
+import pino from 'pino-http';
+
+console.log(`add(1, 2, 3, 4) = ${add(1, 2, 3, 4)}`);
+console.log(`clamp(12, 1, 100) = ${clamp(12, 1, 100)}`);
 
 const app = express();
 
-console.log(`add(1, 2, 3, 4) = ${add(1, 2, 3, 4)}`);
+app.use(
+  pino({
+    prettyPrint: true,
+  }),
+);
 
-console.log(`clamp(12, 1, 100) = ${clamp(12, 1, 100)}`);
+app.get('/', (req, res) => {
+  res.json({ message: 'o hai!' });
+});
 
-app.get('/', (req, res) => res.json({ message: 'o hai!' }));
+const errorHandler: ErrorRequestHandler = (error: unknown, req, res, next) => {
+  if (res.headersSent) {
+    return next(error);
+  }
 
-const port = process.env.PORT || 3000;
+  req.log.error(error as string);
+
+  res.status(500).json({ error });
+};
+
+app.use(errorHandler);
+
+const port = Number(process.env.PORT) || 9;
+
 app.listen(port, () => {
   console.log(`Server is 🚀 on port ${port}`);
 });
